ff9212c46bd7586be16ccb77c479a8e0dfe7907b
Root0073 <2720880732@qq.com>
2026-08-03 23:42:00 +0800

    font page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     #主页
-    path(r'', views.index, name='index')
+    path(r'', views.index, name='index'),
+    path(r'^topics/$', views.topics, name='topics'),
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topics, name='topic')
 ]
